1d45827a6b33a755dc046b390bf4e5a5779519fb
ccbluecc <chanapolpuntuvade1@gmail.com>
2024-10-04 17:09:26 +0700

    header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+</script>
 
 <template>
-  <header>
-    <div class="wrapper text-red-300">Test</div>
-  </header>
+  <Header position="left">
+    <template #icon>
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="./assets/logo.svg"
+        width="125"
+        height="125"
+      />
+    </template>
+    <template #default>
+      <div>
+        <h2 class="text-2xl font-semibold">Vue</h2>
+        <div>
+          A progressive JavaScript framework for building user interfaces
+        </div>
+      </div>
+    </template>
+  </Header>
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { defineProps } from 'vue'
+
+defineProps({
+  position: {
+    type: String,
+    default: 'left',
+    validator(value) {
+      return ['left', 'center', 'right'].includes(value)
+    }
+  }
+})
+</script>
+
+<template>
+  <div
+    class="flex flex-row border border-solid border-gray-300 p-4 rounded-2xl text-white bg-gradient-to-r from-blue-300 to-violet-400 shadow-lg"
+    :class="[
+      { 'items-start': position === 'left' },
+      { 'items-center': position === 'center' },
+      { 'items-end': position === 'right' }
+    ]"
+  >
+    <div class="mr-4 flex-shrink-0">
+      <slot name="icon"></slot>
+    </div>
+    <div class="flex-grow">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
