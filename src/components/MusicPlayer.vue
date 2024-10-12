56dcb98ce2da1a46cebf5bf864cbbf84e2d2034a
pwntnj <natthanan.ppp@gmail.com>
2024-10-12 16:00:58 +0700

    104 - [add] volume control feature in audio player

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -12,6 +12,7 @@ const isPlaying = ref(false)
 const currentTime = ref(0)
 const audioDuration = ref(0)
 const volume = ref(1)
+const volumeBeforeMute = ref(1);
 const isMuted = ref(false)
 
 const playPause = () => {
@@ -35,16 +36,27 @@ const seekAudio = () => {
 const changeVolume = () => {
   if (music.value) {
     music.value.volume = volume.value;
-    isMuted.value = volume.value === 0;
+    isMuted.value = volume.value === 0; 
+    if (!isMuted.value) {
+      volumeBeforeMute.value = volume.value; 
+    }
   }
-}
+};
 
 const toggleMute = () => {
   if (music.value) {
+    if (isMuted.value) {
+      volume.value = volumeBeforeMute.value;
+      music.value.volume = volumeBeforeMute.value;
+    } else {
+      volumeBeforeMute.value = volume.value;
+      volume.value = 0;
+      music.value.volume = 0;
+    }
     isMuted.value = !isMuted.value;
-    music.value.muted = isMuted.value;
   }
-}
+};
+
 
 watch(() => props.previewUrl, (newUrl) => {
   if (music.value) {
@@ -88,13 +100,13 @@ onMounted(() => {
 </script>
 
 <template>
-    <div v-if="previewUrl" class="fixed bottom-0 bg-gradient-to-r from-gray-800 to-gray-900 p-4 rounded-3xl left-1/2 transform -translate-x-1/2  w-full">
+    <div v-if="previewUrl" class="fixed bottom-0 bg-gradient-to-r from-gray-800 to-gray-900 p-4 rounded-3xl left-1/2 transform -translate-x-1/2 w-full">
       <audio ref="music" class="hidden">
         <source :src="previewUrl" type="audio/mp3" />
       </audio>
     
-      <div class="flex flex-col w-full ">
-        <div class="flex items-center text-white text-left -mb-16 mt-3">
+      <div class="flex flex-col w-full">
+        <div class="flex items-center text-white text-left -mb-20 mt-4">
           <img 
             v-if="currentTrack.images && currentTrack.images.length > 0" 
             :src="currentTrack.images[0].url" 
@@ -106,22 +118,30 @@ onMounted(() => {
             <p class="text-sm">{{ currentTrack.artists[0].name }}</p>  
           </div>
         </div>
+
         <div class="flex items-center justify-center w-full mb-4"> 
           <button @click="playPause" class="rounded-full bg-white p-2 hover:bg-gray-200">
             <img :src="isPlaying ? '/src/assets/pause.png' : '/src/assets/play.png'" class="w-8 h-8" />
           </button>
         </div>
-  
-        <div class="flex items-center space-x-4 w-full text-white justify-center">
-          <span>{{ Math.floor(currentTime / 60) }}:{{ ('0' + Math.floor(currentTime % 60)).slice(-2) }}</span>
-          <input type="range" min="0" :max="audioDuration" v-model="currentTime" @input="seekAudio" 
-            class="h-1 bg-gray-700 rounded-full cursor-pointer w-2/6" />
-          <span>{{ Math.floor((audioDuration - currentTime) / 60) }}:{{ ('0' + Math.floor((audioDuration - currentTime) % 60)).slice(-2) }}</span>
+ 
+        <div class="flex items-center w-full text-white">
+            <div class="flex items-center space-x-4 justify-center flex-grow ml-40">
+                <span>{{ Math.floor(currentTime / 60) }}:{{ ('0' + Math.floor(currentTime % 60)).slice(-2) }}</span>
+                <input type="range" min="0" :max="audioDuration" v-model="currentTime" @input="seekAudio" 
+                class="h-1 bg-gray-700 rounded-full cursor-pointer w-96" />
+                <span>{{ Math.floor((audioDuration - currentTime) / 60) }}:{{ ('0' + Math.floor((audioDuration - currentTime) % 60)).slice(-2) }}</span>
+            </div>
+            <div class="flex items-center space-x-4 mr-4">
+                <img :src="volume === 0 || isMuted ? '/src/assets/volumemute.png' : '/src/assets/volume.png'" 
+                class="w-6 h-6 cursor-pointer" @click="toggleMute" />
+                <input type="range" min="0" max="1" step="0.01" v-model="volume" @input="changeVolume" 
+                class="h-1 bg-gray-700 rounded-full cursor-pointer w-30" />
+            </div>
         </div>
       </div>
     </div>
-  </template>
-  
+</template>
 
 <style scoped>
 
